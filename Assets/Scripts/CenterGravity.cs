3af526ea04335704ea280f77876e5eaa47a4f45f
joel_barr <joelbarr1220@gmail.com>
2019-01-25 23:03:32 -0700

    Added a behaviour script that pulls the gameobject to the center of the world

---
+++ Assets/Scripts/CenterGravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterGravity : MonoBehaviour {
+    [SerializeField] Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        if (!rigidbody)
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(transform.position.normalized * -9.81f);
+    }
+}
