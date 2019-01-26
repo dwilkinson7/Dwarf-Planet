aa6014b63ea257a077a1ab03cddbaaa15282860c
joel_barr <joelbarr1220@gmail.com>
2019-01-26 10:24:23 -0700

    no message

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float MoveSpeed = 1f;
+    public float RotationSpeed = 1f;
+    public Transform Target;
+    public Vector3 Offset;
+    public Vector3 LookatOffset;
+
+    Quaternion desiredRotation;
+    Vector3 desiredLocation;
+    float distanceToDestination;
+
+    private void FixedUpdate()
+    {
+        desiredLocation = Target.TransformPoint(Offset);
+        distanceToDestination = (desiredLocation - transform.position).sqrMagnitude;
+        transform.position = Vector3.Lerp(transform.position, desiredLocation, distanceToDestination * MoveSpeed * Time.fixedDeltaTime);
+        desiredRotation = Quaternion.LookRotation((Target.position + LookatOffset) - transform.position, Target.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, RotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CraftBoxes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftBoxes : MonoBehaviour {
+    public float distance;
+    public float delay;
+    public Transform box;
+    public int count;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(Spawn());	
+	}
+
+    IEnumerator Spawn()
+    {
+        Vector3 location;
+        Transform nuBox;
+        for (int i = 0; i < count; i++)
+        {
+            location = Random.onUnitSphere * distance;
+            
+            nuBox = Instantiate(box);
+            nuBox.position = location;
+            nuBox.up = location - transform.position;
+            Debug.Log(i);
+            if (i % 10 == 0)
+                yield return new WaitForSeconds(delay);
+        }
+    }
+}
