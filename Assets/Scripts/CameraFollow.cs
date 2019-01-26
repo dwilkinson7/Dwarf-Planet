e77a1351dbcee972f1277adfe43dfafc1ba83d27
joel_barr <joelbarr1220@gmail.com>
2019-01-26 14:33:33 -0700

    Updating Camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -12,14 +12,14 @@ public class CameraFollow : MonoBehaviour
 
     Quaternion desiredRotation;
     Vector3 desiredLocation;
-    float distanceToDestination;
+    Vector3 _toDestination;
 
     private void FixedUpdate()
     {
         desiredLocation = Target.TransformPoint(Offset);
-        distanceToDestination = (desiredLocation - transform.position).sqrMagnitude;
-        transform.position = Vector3.Lerp(transform.position, desiredLocation, distanceToDestination * MoveSpeed * Time.fixedDeltaTime);
+        _toDestination = desiredLocation - transform.position;
+        transform.position += Vector3.ClampMagnitude(_toDestination, MoveSpeed);
         desiredRotation = Quaternion.LookRotation((Target.position + LookatOffset) - transform.position, Target.position);
-        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, RotationSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, RotationSpeed * Time.fixedDeltaTime);
     }
 }
