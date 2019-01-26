8ebb094da35f9926dd7fb2631d6e2102ac9601af
Edward Rochester <edward.m.rochester@gmail.com>
2019-01-26 13:43:42 -0700

    added pickup scene

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    Dictionary<string, int> items = new Dictionary<string, int>();
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addItem(string type, int amount)
+    {
+        items.TryGetValue()
+        items.Add(type, amount);
+        Debug.Log("Added: " + type + " of amount: " + amount.ToString());
+    }
+}
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public int amount = 1;
+    string type = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * 5);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            col.gameObject.GetComponent<Inventory>().addItem(type, amount);
+        }
+    }
+}
