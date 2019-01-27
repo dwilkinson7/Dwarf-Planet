9dce6550e273c2bda33d546c0ea06fd04e146542
dwilkinson7 <dw15@ualberta.ca>
2019-01-27 12:03:08 -0700

    Rain effect

---
+++ Assets/Scripts/RainGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainGenerator : MonoBehaviour
+{
+    public GameObject rainDrop;
+    private int rainProbability = 20;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < rainProbability)
+        {
+            Object.Destroy(Instantiate(rainDrop, transform.position, Quaternion.identity), 15.0f); ;
+        }
+    }
+}
