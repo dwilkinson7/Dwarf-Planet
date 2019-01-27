9f32649f93eaa19be0ea750d1f2c653458fa9bc5
dwilkinson7 <dw15@ualberta.ca>
2019-01-27 11:20:04 -0700

    Added sun to scene w prefab

---
+++ Assets/sunrotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunrotation : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.left, 5 * Time.deltaTime);
+    }
+}
