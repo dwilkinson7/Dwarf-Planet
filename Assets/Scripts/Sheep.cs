f1f82d9e5036a27f9f607c73f1e9b2d4413df0e0
Edward Rochester <edward.m.rochester@gmail.com>
2019-01-27 14:27:21 -0700

    Sheeps!

---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public GameObject sheep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(sheep, transform.position + 55 * transform.up.normalized, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+
+    }
+
+}
---
+++ Assets/SheepRun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepRun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(transform.up.x * 0.06f, transform.up.y * 0.06f + transform.up.z * 0.06f);
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * 20);
+    }
+}
