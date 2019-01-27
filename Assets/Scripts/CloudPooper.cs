10241afcbe56cc895a15b30d1c7025d76aed8668
Edward Rochester <edward.m.rochester@gmail.com>
2019-01-27 11:48:49 -0700

    Added cloud with flight for it

---
+++ Assets/Scripts/CloudPooper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudPooper : MonoBehaviour
+{
+
+    public GameObject pivot;
+    public float magnitude = 10;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(pivot.transform.position, transform.right, speed * Time.deltaTime);
+        transform.Rotate(transform.up * Time.deltaTime * magnitude * Random.Range(-1f, 1f), Space.Self);
+
+    }
+}
